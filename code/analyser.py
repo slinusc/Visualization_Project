ddada8ff43cf80c6af7824fe85e9881166614934
Linus Stuhlmann <stuhllin@students.zhaw.ch>
2023-05-09 19:14:46 +0200

    create analyser & word_preprocessing

---
+++ code/analyser.py
@@ -0,0 +1,76 @@
+import spacy
+import difflib
+import pandas as pd
+import matplotlib.pyplot as plt
+from collections import Counter
+from textblob_de import TextBlobDE as TextBlob
+from wordcloud import WordCloud
+
+class HeadlineAnalyser:
+
+    def __init__(self, headlines: list):
+        self.headlines = headlines
+        self.nlp_de = spacy.load('de_core_news_sm')
+        self.topics = ["Politik", "Wirtschaft", "Sport", "Kultur", "Wissenschaft", "Technik", "Gesundheit", "Lifestyle"]
+
+    def get_most_common_entities(self, n=10):
+        entities = Counter()
+        for text in self.headlines:
+            doc = self.nlp_de(text)
+            person_names = [ent.text.lower() for ent in doc.ents if ent.label_ == 'PER']
+            full_names = [name.replace("'s", "").title() for name in person_names]
+            entities.update(full_names)
+
+            nouns = [token.text.lower() for token in doc if token.pos_ == 'NOUN']
+            capitalized_nouns = [noun.title() for noun in nouns]
+            entities.update(capitalized_nouns)
+
+        return entities.most_common(n)
+
+    def get_categorized_headlines(self):
+        categorized_headlines = {topic: [] for topic in self.topics}
+
+        for headline in self.headlines:
+            similarities = [difflib.SequenceMatcher(None, headline.lower(), topic.lower()).ratio() for topic in
+                            self.topics]
+            best_match_topic = self.topics[similarities.index(max(similarities))]
+            categorized_headlines[best_match_topic].append(headline)
+
+        return categorized_headlines
+
+    def get_topic_sentiments(self):
+        topic_sentiments = {}
+        for topic, headlines in self.get_categorized_headlines().items():
+            sentiment_scores = []
+            for headline in headlines:
+                headline_blob = TextBlob(headline)
+                sentiment_scores.append(headline_blob.sentiment.polarity)
+            topic_sentiments[topic] = sentiment_scores
+        return topic_sentiments
+
+    def get_word_cluster(self, words):
+        text = " ".join(schlagzeile for schlagzeile in words)
+        wordcloud = WordCloud(collocations=False, background_color="white").generate(text)
+        # Zeigt die Wortwolke mit Matplotlib an
+        plt.figure(figsize=(10, 5))
+        plt.imshow(wordcloud, interpolation="bilinear")
+        plt.axis("off")
+        plt.show()
+
+if __name__ == "__main__":
+    import pandas as pd
+    headlines = [
+    "Bundesregierung plant neue Klimaschutzmaßnahmen",
+    "Zahlreiche Demonstrationen gegen Pandemie-Maßnahmen",
+    "Start-up revolutioniert Energiegewinnung mit neuem Verfahren",
+    "Deutschland gewinnt im Elfmeterschießen gegen Frankreich",
+    "Künstliche Intelligenz eröffnet neue Perspektiven in der Medizin",
+    "Rekordverdächtiger Sturm verwüstet Teile von Norddeutschland",
+    "Impfstoff-Hersteller kündigt Verbesserung bei Impfstoffproduktion an",
+    "Weltweite Proteste gegen Klimawandel zeigen Wirkung",
+    "Neue Studie zeigt: Gesunde Ernährung ist einfacher als gedacht",
+    "Hitzewelle in Südeuropa führt zu steigenden Touristenzahlen"]
+    analyser = HeadlineAnalyser(headlines)
+    words = analyser.get_most_common_entities(50)
+    words = [tupel[0] for tupel in words]
+    analyser.get_word_cluster(words)
---
+++ code/word_preprocessing.py
@@ -0,0 +1,50 @@
+import string
+from nltk.stem.snowball import SnowballStemmer
+import spacy
+
+class headline_preprocessing:
+    def __init__(self, text):
+        self.text = text
+        self.nlp_de = spacy.load('de_core_news_sm')
+
+    def preprocess_text(self, text):
+        text = self.remove_punctuation(text)
+        text = self.remove_stopwords(text)
+        text = self.lemmatize_words(text)
+        return text
+
+    def preprocess_headlines(self, headlines):
+        preprocessed_headlines = [self.preprocess_text(headline) for headline in headlines]
+        return preprocessed_headlines
+
+    def remove_punctuation(self, text):
+        translator = str.maketrans("", "", string.punctuation)
+        return text.translate(translator)
+
+    def remove_stopwords(self, text):
+        doc = self.nlp_de(text)
+        filtered_tokens = [token for token in doc if not token.is_stop]
+        filtered_text = " ".join(token.text for token in filtered_tokens)
+        return filtered_text
+
+    def lemmatize_words(self, text):
+        doc = self.nlp_de(text)
+        lemmatized_tokens = [token.lemma_ for token in doc]
+        lemmatized_text = " ".join(lemmatized_tokens)
+        return lemmatized_text
+
+
+if __name__ == "__main__":
+    import pandas as pd
+
+    headlines = pd.read_csv("C:/Users/linus/Downloads/daten_aijan_filtered.csv")
+
+    # Verwenden Sie die Klasse headline_preprocessing, um eine einzelne Schlagzeile zu bereinigen
+    single_headline = headlines['titel'][0]
+    preprocessor = headline_preprocessing(single_headline)
+    preprocessed_single_headline = preprocessor.preprocess_text(single_headline)
+    print("Einzelne bereinigte Schlagzeile:", preprocessed_single_headline)
+
+    # Verwenden Sie die Klasse headline_preprocessing, um eine Liste von Schlagzeilen zu bereinigen
+    all_preprocessed_headlines = [preprocessor.preprocess_text(headline) for headline in headlines['titel']]
+    print("Alle bereinigten Schlagzeilen:", all_preprocessed_headlines)
