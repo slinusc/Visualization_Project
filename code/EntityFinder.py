ffb7e7319f9c2000880000c2cc3037ecbf91fcb5
Linus Stuhlmann <stuhllin@students.zhaw.ch>
2023-05-13 16:58:34 +0200

    Update EntityFinder.py

--- code/EntityFinder.py
+++ code/EntityFinder.py
@@ -3,14 +3,15 @@ from collections import Counter
 
 class EntityFinder:
 
-    def __init__(self, headlines: list):
+    def __init__(self):
         self.nlp_de = spacy.load('de_core_news_sm')
         self.topics = ["Politik", "Wirtschaft", "Sport", "Kultur", "Wissenschaft", "Technik"]
 
-    def get_most_common_entities(self, n=3):
+    def get_most_common_entities(self, text, n=3):
         entities = Counter()
-        for text in self.headlines:
-            doc = self.nlp_de(text)
+        for word in text:
+            doc = self.nlp_de(word)
+
             person_names = [ent.text.lower() for ent in doc.ents if ent.label_ == 'PER']
             full_names = [name.replace("'s", "").title() for name in person_names]
             entities.update(full_names)
@@ -25,6 +26,7 @@ class EntityFinder:
 if __name__ == "__main__":
 
     analyser = EntityFinder()
-
-    words = analyser.get_most_common_entities(20)
+    text = ["Schweden", "du", "Haus", "Adrian", "USA", "Schweiz"]
+    words = analyser.get_most_common_entities(text)
+    print(words)
 
