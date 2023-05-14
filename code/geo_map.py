1e9e67d66af090ed282fe949631a66e39030708b
Mock Andrés (mockand1) <mockand1@students.zhaw.ch>
2023-05-14 20:09:05 +0200

    worldmap whoop whoop

--- code/geo_map.py
+++ code/geo_map.py
@@ -1,87 +1,33 @@
 import pandas as pd
 import plotly.express as px
-import geopandas as gpd
-import warnings
-
-warnings.filterwarnings('ignore')
-from fuzzywuzzy import process
-
+import time
+import ast
 
 # TODO: percentage Bar anpassen
-
-def laender_aufbereitung():
-    # Liste mit Ländern auf Deutsch
-    with open("C:/Users/andre/Git_ripository/visualization_project/data/de_laender.csv",
-              encoding="UTF-8") as laender_de:
-        # deutsch_list söllen länder sein, die übergeben werden
-        deutsch_list = [line.strip() for line in
-                        laender_de.readlines()]  # Inhalt der Datei zeilenweise in Liste speichern
-
-    # Länderliste auf englisch, alle Länder die in Grafik erkannt werden könnten.
-    world = gpd.read_file(gpd.datasets.get_path('naturalearth_lowres'))
-    english_list = world["name"].tolist()  # Spalte "name" als Liste ausgeben
-
-    result_dict = {}
-
-    for country in deutsch_list:
-        match, score = process.extractOne(country, english_list)
-        if score > 70:  # Schwellenwert KÖNNTE anpasst werden. BITTE SO LASSEN
-            result_dict[country] = match
-
-    # Für dieses Projekt wichtige Länder, die von fuzzywuzzy falsch oder nicht erkannt wurde.
-    dict_nicht_erkannt = {'Frankreich': 'France', 'Schweiz': 'Switzerland', 'USA': 'United States of America',
-                          'Spanien': 'Spain', 'Deutschland': 'Germany', 'Italien': 'Italy', 'Österreich': 'Austria',
-                          'England': 'United Kingdom', 'Kroatien': 'Croatia', 'Süd Afrika': 'South Africa',
-                          'Dominikanische Republik': 'Dominican Rep.', 'Griechenland': 'Greece',
-                          'Weissrussland': 'Belarus', 'Süd Sudan': 'S. Sudan',
-                          'Demokratische Republik Kongo': 'Dem. Rep. Congo', 'Kongo': 'Congo', 'Birma': 'Myanmar',
-                          'Kolumbien': 'Colombia', 'Tschechische Republik': 'Czechia', 'Rumänien': 'Romania',
-                          'Norwegen': 'Norway', 'Tunesien': 'Tunisia', 'Kambodscha': 'Cambodia',
-                          'Island': 'Iceland', 'Litauen': 'Lithuania', 'Estland': 'Estonia', 'lettland': 'Latvia'}
-    result_dict.update(dict_nicht_erkannt)
-
-    # Daten, die so der Weltkartengrafik übergeben werden können.
-    data = {'Country': []}
-
-    for key, value in result_dict.items():
-        data['Country'].append(value)
-    return data
+def get_countries(date):
+    start_time_countries = time.time()
+    selected_string = data_country.loc[data_country['datum'] == date, 'Country'].values[0]
+    selected_dict = ast.literal_eval(selected_string)  # Umwandlung des String in ein Dictionary
+    df = pd.DataFrame(selected_dict)
+    print("get_countries", time.time() - start_time_countries)
+    return df
 
 
 def weltkarte(data):
+    start_time_weltkarte = time.time()
     df = pd.DataFrame(data)
 
-    # Anzahl von jedem Land
-    df['Count'] = df.groupby('Country')['Country'].transform('count')
-
-    # Duplikatentfernung
-    df = df.drop_duplicates()
-
-    # Dataimport aus naturalearth_lowres um Koordinaten zu erhalten
-    world = gpd.read_file(gpd.datasets.get_path('naturalearth_lowres'))
-
-    # Koordinaten jedem land hinzufügen
-    merged = df.merge(world, how="left", left_on='Country', right_on='name')
-
-    # Berechung der Schwerpunktkoordinaten
-    world['representative_point'] = world['geometry'].representative_point()
-    world['lon'] = world['representative_point'].x
-    world['lat'] = world['representative_point'].y
-
-    # Merge der DataFrames, basierend auf der Spalte "name" und "Country"
-    merged = merged.merge(world[['name', 'lon', 'lat']], how='left', left_on='Country', right_on='name')
-
-    # Przentuale Anzahl Auftreten für Mrkergrössenverhältnisse
-    merged['Percentage'] = 100 * merged['Count'] / merged['Count'].sum()
+    # Duplikate zusammenfassen und 'Count'-Werte summieren
+    df = df.groupby('Country').sum().reset_index()
 
     # Daten für die Kartenvisualisierung vorbereiten
-    fig = px.choropleth(merged, locations='name_x', locationmode='country names',
-                        color='Percentage', hover_name='Country',
-                        hover_data=['Count', 'Percentage'],
-                        projection='natural earth',
-                        color_continuous_scale='greens')
+    fig = px.choropleth(df, locations='Country',
+                        locationmode='country names',
+                        color='Count',
+                        title='Count by Country',
+                        color_continuous_scale=px.colors.sequential.Greens)
 
-    # Kartenlayout festlegen
+    # Kartenlayout
     fig.update_geos(showcountries=True, countrycolor="darkgrey", showocean=True, oceancolor="lightblue",
                     showland=True, landcolor="white", showframe=False)
 
@@ -92,13 +38,38 @@ def weltkarte(data):
             scope='world',
             projection_type='natural earth'
         ),
-        width=900,  # Anpassen der Breite der Figur
-        height=700  # Anpassen der Höhe der Figur
+        width=900,
+        height=700
     )
 
+    # Formatierung anpassen Kacheln
+    fig.update_traces(hovertemplate='<b>Land</b>: %{location}<br><b>Anzahl</b>: %{z}')
+    # Beschriftung ändern des Farbbalkens
+    fig.update_layout(coloraxis_colorbar_title='Anzahl')
+
     # Karte anzeigen
+    print("weltkarte", time.time() - start_time_weltkarte)
     fig.show()
 
 
+    # TODO evtl das integrieren???????
+    """
+    # Barplot für Verteilung
+    bar_df = df.sort_values(by='Count', ascending=False).head(10)  # Nur die 10 größten Werte
+    fig_bar = px.bar(bar_df, x='Country', y='Count', title='Verteilung des Counts pro Land',
+                     color='Count', color_continuous_scale=px.colors.sequential.Greens)
+    fig_bar.update_layout(xaxis_title='Land', yaxis_title='Count')
+    fig_bar.update_traces(marker=dict(color='green', width=1),
+                          selector=dict(type='bar'))
+    fig_bar.show()
+    """
+
+
 if __name__ == '__main__':
-    weltkarte(laender_aufbereitung())
+    start_time = time.time()
+    data_country = pd.read_csv(
+        'C:/Users/andre/Desktop/ZHAW Unterricht (Nicht Cloud)/Semester 2/Visualisation and Data Science Storytelling/Semestertask/data_country.csv')
+    weltkarte(get_countries("2022-01-12"))
+    completed_time = time.time()
+    final_tile = completed_time - start_time
+    print("Ladezeit für Weltkarte:", final_tile, "Sekunden")
