d693f429977ce8b0c7f08f202bdd2aa618ff88e6
Linus Stuhlmann <stuhllin@students.zhaw.ch>
2023-06-12 10:10:40 +0200

    Update 1_Geografische_Analyse.py

--- app/pages/1_Geografische_Analyse.py
+++ app/pages/1_Geografische_Analyse.py
@@ -3,11 +3,13 @@ import pandas as pd
 import holoviews as hv
 import sys
 
-sys.path.insert(0, 'C:/Users/andre/Git_ripository/visualization_project/app/classes')
-import relation_chord_chart as rcc
-import geo_map as gm
-import sentiment_plot as sp
-from top_pers_coun import StackedBarPlot
+#sys.path.insert(0, 'C:/Users/linus/OneDrive/BSc_Data_Science/Semester_2/Data_Visualisation/'
+                   #'visualization_project/app/classes')
+
+from .classes import relation_chord_chart as rcc
+from .classes import geo_map as gm
+from .classes import sentiment_plot as sp
+from .classes import top_pers_coun as tpc
 
 
 @st.cache_data
@@ -100,7 +102,7 @@ with left_col:
 
 # TOP 10 LÄNDER
 with right_col:
-    bar_chart = StackedBarPlot(filtered_df, filter='country')
+    bar_chart = tpc.StackedBarPlot(filtered_df, filter='country')
     fig = bar_chart.plot()
     st.button('ℹ️', help="Das Balkendiagramm zeigt die absolute Häufigkeit der genannten Länder,"
                          "horizontal gestapelt erkennt man die Kategorien der Artikel, in welchem sie gennant wurden. "
